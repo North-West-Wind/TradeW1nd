4f021767c50ccb3d98822bc257bdcd59e4d90e29
NorthWestWind <wsyn148@gmail.com>
2021-10-08 18:21:24 +0800

    Update function.ts

--- src/function.ts
+++ src/function.ts
@@ -281,11 +281,11 @@ export function messagePrefix(message: Discord.Message, client: NorthClient): st
 export function humanDurationToNum(duration: string) {
     const splitted = duration.split(".");
     const rest = splitted[0];
-    const splitted1 = rest.split(":");
+    const splitted1 = rest.split(":").reverse();
     var sec = 0;
-    for (let i = splitted1.length - 1; i > 0; i--) {
+    for (let i = 0; i < splitted1.length; i++) {
         let parsed;
-        if (isNaN(parsed = parseInt(splitted1[1]))) continue;
+        if (isNaN(parsed = parseInt(splitted1[i]))) continue;
         sec += parsed * Math.pow(60, i);
     }
     return sec;
