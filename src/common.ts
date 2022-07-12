30ba983192f12f33da0106ac2a225647fe979c6d
North-West-Wind <wsyn148@gmail.com>
2022-07-13 01:15:49 +0800

    correct directory

--- src/common.ts
+++ src/common.ts
@@ -46,7 +46,7 @@ export default async (client: NorthClient) => {
   console.error = (message: string, ...data: any[]) => logger.error(message, ...data);
   console.debug = (message: string, ...data: any[]) => logger.debug(message, ...data);
   console.trace = (message: string, ...data: any[]) => logger.trace(message, ...data);
-  const commandFiles = deepReaddir("./out/src/commands").filter(file => file.endsWith(".js"));
+  const commandFiles = deepReaddir("./out/commands").filter(file => file.endsWith(".js"));
   for (const file of commandFiles) {
     const command = <SlashCommand>(await import(file)).default;
     NorthClient.storage.commands.set(command.name, command);
